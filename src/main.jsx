aafb0fed2a4095e36b050ff54dbe329fc5a99b8d
ksc1728 <139704073+ksc1728@users.noreply.github.com>
2025-11-12 19:42:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App.jsx';
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Analytics/>
   </BrowserRouter>
 )
