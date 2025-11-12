28f1dbe790187479e12e84abc0212dc9223a832e
ksc1728 <139704073+ksc1728@users.noreply.github.com>
2025-11-12 20:40:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/quiz' element={<Quiz/>}/>
+      <Route path='*' element={<Home/>}/>
     </Routes>
   )
 }
